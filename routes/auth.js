a4a3d8122475c96a37c3f5a477b5fb3b108d1458
Amankr705 <ak.1000aman@gmail.com>
2023-03-09 22:55:48 +0530

    fixes

--- routes/auth.js
+++ routes/auth.js
@@ -15,16 +15,17 @@ router.post('/createuser', [
     body('email', 'Enter a valid unique email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
 ], async (req, res) => {
+    let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ success, errors: errors.array() });
     }
     // check whether the user with same email exists already
     try {
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: "Sorry a user with this email already exists" })
+            return res.status(400).json({ success, error: "Sorry a user with this email already exists" })
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -46,7 +47,8 @@ router.post('/createuser', [
         // console.log(jwtData);
 
         // res.json(user);
-        res.json({ authtoken })
+        success = true;
+        res.json({ success, authtoken })
         //catch errors
     } catch (error) {
         console.error(error.message);
